eca093281c5abda22fbadefec036930cc2ec7beb
Nahid <m.h.nahid431@gmail.com>
2026-05-04 21:28:12 +0100

    fix stripe init when key not set

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,10 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-04-22.dahlia",
-});
+export const stripe = process.env.STRIPE_SECRET_KEY
+  ? new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: "2026-04-22.dahlia",
+    })
+  : null;
 
 export const PLANS = {
   PRO: {
